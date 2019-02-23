5a8220f86114da7dc9323a5034400dbd1e25b074
zien <sheenazien08@gmail.com>
2019-02-23 17:26:05 +0700

    new fiture

--- sys/Controller.php
+++ sys/Controller.php
@@ -2,5 +2,17 @@
 namespace ZL;
 
 class Controller {
+    protected $input;
+    protected $redirect;
+    protected $session;
 
+    /**
+     * Controller constructor.
+     */
+    public function __construct()
+    {
+        $this->input = new Input();
+        $this->redirect = new Redirect();
+        $this->session = new Session();
+    }
 }
